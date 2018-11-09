b5f437c88e1c30f93d1af5fcbdc4ec998039ef9c
Martin Villalba <villalba@7c0h>
2018-11-09 20:23:57 +0100

    Added tests cases to the backend

--- backend.py
+++ backend.py
@@ -4,3 +4,19 @@ import argparse
 
 # Parse arguments for the program
 parser = argparse.ArgumentParser(description='Performs queries to the calendar API')
+
+# These are the list of operations that the GUI supports
+# We ask for exactly one of them
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('--see_slots', dest='see_slots', metavar='USER_ID',
+                   help='See the available slots for the selected user')
+group.add_argument('--meeting', dest='meeting_members', metavar='USER_ID', nargs='+',
+                   help='Show possible meeeting dates. The first argument is the interviewee.')
+
+args = parser.parse_args()
+
+# Time to check which operation is the GUI requesting
+if args.see_slots:
+    print("Show slots")
+elif args.meeting_members:
+    print("Organize a meeting with: {}".format(args.meeting_members))
--- flask_server.py
+++ flask_server.py
@@ -4,6 +4,7 @@ from flask import Flask, request
 # To run: FLASK_APP=flask_server.py python3 -m flask run --host=0.0.0.0
 app = Flask(__name__)
 
+
 @app.route("/")
 def serve():
     param = request.args.get('request')
