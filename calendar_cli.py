db4154cd81d0cb1a1d6494cd7d4662c1c2631bdf
Martin Villalba <villalba@home>
2018-11-10 01:00:48 +0100

    Added an incomplete version of the 'add_slot' functionality

--- calendar_cli.py
+++ calendar_cli.py
@@ -13,6 +13,8 @@ parser = argparse.ArgumentParser(description='Performs queries to the calendar A
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument('--add_user', dest='add_user', metavar='STR', nargs=2,
                    help='Add a user to the database')
+group.add_argument('--add_slot', dest='add_slot', metavar='STR', nargs=3,
+                   help='Add a range of slots to the database')
 group.add_argument('--see_slots', dest='see_slots', metavar='USER_ID',
                    help='See the available slots for the selected user')
 group.add_argument('--meeting', dest='meeting_members', metavar='USER_ID', nargs='+',
@@ -25,6 +27,12 @@ cal = Calendar('database.db')
 if args.add_user:
     # Add a user to the database
     retval = json.loads(cal.add_user(args.add_user[0], args.add_user[1]))
+    if retval['code'] != 0:
+        sys.stderr.write("Error adding user: {}".format(retval['desc']))
+elif args.add_slot:
+    # Add a slot to the database
+    # TODO: make it easier to enter a date
+    retval = json.loads(cal.add_slots(args.add_slot[0], args.add_slot[1], args.add_slot[2]))
     if retval['code'] != 0:
         sys.stderr.write("Error adding user: {}".format(retval['desc']))
 elif args.see_slots:
